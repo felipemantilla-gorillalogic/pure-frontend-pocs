3330e9d32dd87224dbcd0d82ecda61af6986478b
Felipe Mantilla <andres.mantilla@gorillalogic.com>
2024-07-18 09:50:51 -0500

    update

--- src/views/TinyMCE.vue
+++ src/views/TinyMCE.vue
@@ -1,112 +1,74 @@
 <template>
     <div class="container">
-        <div class="tiny-editor">
-            <editor api-key="22a5hpysl6pcfcctei3v4pxthc607lx40ucaa2rbrltzbkld" :init="config" />
-        </div>
+      <div class="tiny-editor">
+        <Editor api-key="22a5hpysl6pcfcctei3v4pxthc607lx40ucaa2rbrltzbkld" :init="config" />
+      </div>
     </div>
     <Teleport to="body">
-        <!-- use the modal component, pass in the prop -->
-        <modal :show="showModal" @close="showModal = false">
-            <template #header>
-                <h3>Paste file url</h3>
-            </template>
-            <template #body>
-                <input type="text" v-model="fileUrl" />
-            </template>
-            <template #footer>
-                <button @click="showModal = false">Cancel</button>
-                <button @click="handleUpload">Upload</button>
-            </template>
-        </modal>
+      <Modal :show="showModal" @close="closeModal">
+        <template #header>
+          <h3>Paste file URL</h3>
+        </template>
+        <template #body>
+          <input type="text" v-model="fileUrl" />
+        </template>
+        <template #footer>
+          <button @click="closeModal">Cancel</button>
+          <button @click="handleUpload">Upload</button>
+        </template>
+      </Modal>
     </Teleport>
-</template>
-
-<script setup>
-
-import Editor from '@tinymce/tinymce-vue';
-import Modal from '../components/Modal.vue'
-import { ref } from 'vue'
-
-const showModal = ref(false)
-
-const fileUrl = ref('')
-
-const config = {
-    // menubar: false,
-    file_picker_callback: (callback, value, meta) => {
-
-    },
+  </template>
+  
+  <script setup>
+  import { ref } from 'vue';
+  import Editor from '@tinymce/tinymce-vue';
+  import Modal from '../components/Modal.vue';
+  
+  const showModal = ref(false);
+  const fileUrl = ref('');
+  
+  const config = {
     plugins: 'customToolbarButton link file',
     toolbar: 'customToolbarButton',
-    setup: () => {
-        console.log(window.tinymce.PluginManager)
-
-        window.tinymce.PluginManager.add('customToolbarButton', customToolbarButton)
-    }
-}
-
-const handleUpload = () => {
-    console.log(fileUrl.value)
-    // fetch(fileUrl.value)
-    //     .then(response => response.blob())
-    //     .then(blob => {
-    //         // Create a file object from the fetched blob
-
-    //         console.log(blob)
-
-    //         const file = new File([blob], `file.${blob.type.split('/')[1]}`, { type: blob.type });
-
-    //         // Convert the file object to a data URL
-    //         const reader = new FileReader();
-    //         reader.onloadend = () => {
-    //             const dataUrl = reader.result;
-
-    //             // Attach the file to TinyMCE
-    //             const editor = window.tinymce.activeEditor; // Access the active editor instance
-    //             const tag = `<a href="${dataUrl}" download>Download File</a>`
-
-    //             console.log(tag)
-    //             editor.insertContent(tag);
-    //         };
-    //         reader.readAsDataURL(file);
-    //     })
-    //     .catch(error => {
-    //         console.error('Error fetching file:', error);
-    //     });
-
-    const editor = window.tinymce.activeEditor; // Access the active editor instance
-    // const tag = `<a href="${fileUrl.value}" >Download File</a>`
-    const tag = `<a href="${fileUrl.value}" target="_blank" class="file-button">Download File</a>`
+    setup: (editor) => {
+      window.tinymce.PluginManager.add('customToolbarButton', customToolbarButton);
+    },
+  };
+  
+  const closeModal = () => {
+    showModal.value = false;
+  };
+  
+  const handleUpload = () => {
+    const editor = window.tinymce.activeEditor;
+    const tag = `<a href="${fileUrl.value}" target="_blank" class="file-button">Download File</a>`;
     editor.insertContent(tag);
-
-    showModal.value = false
-}
-
-
-const customToolbarButton = (editor) => {
+    closeModal();
+  };
+  
+  const customToolbarButton = (editor) => {
     editor.ui.registry.addButton('customToolbarButton', {
-        text: 'Attach file',
-        icon: 'attach',
-        onAction: () => {
-            console.log('clicked')
-            showModal.value = true
-        }
-    })
-}
-
-
-</script>
-
-<style scoped>
-.tiny-editor {
+      text: 'Attach file',
+      icon: 'attach',
+      onAction: () => {
+        showModal.value = true;
+      },
+    });
+  };
+  </script>
+  
+  <style scoped>
+  .tiny-editor {
     min-width: 80vw;
-}
-
-.container {
+  }
+  
+  .container {
     width: 100vw;
     height: 100vh;
     display: flex;
     justify-content: center;
     align-items: center;
-}
-</style>
+  }
+  </style>
+  
